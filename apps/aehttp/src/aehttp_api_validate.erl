b6323b10804f54c03dbd743ab5d19363c09d857b
Dimitar Ivanov <dimitar.p.ivanov@gmail.com>
2021-02-26 13:51:37 +0200

    WIP

--- apps/aehttp/src/aehttp_api_validate.erl
+++ apps/aehttp/src/aehttp_api_validate.erl
@@ -147,7 +147,8 @@ prepare_param([ Rule | Rules ], Value, Name, Validator) ->
 prepare_param_({"in", _}, _, _, _) -> ok;
 prepare_param_({"name", _}, _, _, _) -> ok;
 prepare_param_({"description", _}, _, _, _) -> ok;
-prepare_param_({"default", _}, _, _, _) -> ok;
+prepare_param_({"default", Default}, undefined, _, _) -> {ok, Default};
+prepare_param_({"default", _Default}, _, _, _) -> ok;
 % required
 prepare_param_({"required",true}, undefined, Name, _) -> param_error(required, Name);
 prepare_param_({"required",_}, _, _, _) -> ok;
